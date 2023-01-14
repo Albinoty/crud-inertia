50b184eb9e147588e183e5e0505e22d8cc7608a9
albinoty <albinotnoty@gmail.com>
2023-01-15 00:14:08 +0100

    Setup Inertia + First Page in React

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
     ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
+        return pages[`./Pages/${name}.jsx`]
+      },
 });
